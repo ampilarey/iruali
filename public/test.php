510bc3955d35dcfe4dbf7063a58829c4c032b1f0
Your Name <your-email@example.com>
2025-07-12 13:59:56 +0500

    Clean up temporary debug files

--- public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Test file is working!";
-echo "<br>Current time: " . date('Y-m-d H:i:s');
-echo "<br>PHP version: " . PHP_VERSION;
-echo "<br>Document root: " . ($_SERVER['DOCUMENT_ROOT'] ?? 'Not set');
-echo "<br>Script path: " . __FILE__;
-?> 

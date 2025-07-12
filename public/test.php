4473ad24c81ac0a9d4bd276caa250870da20d031
Your Name <your-email@example.com>
2025-07-12 13:53:45 +0500

    Add simple test file for debugging 404 issue

--- public/test.php
+++ public/test.php
@@ -1,2 +1,7 @@
 <?php
-phpinfo(); 
+echo "Test file is working!";
+echo "<br>Current time: " . date('Y-m-d H:i:s');
+echo "<br>PHP version: " . PHP_VERSION;
+echo "<br>Document root: " . ($_SERVER['DOCUMENT_ROOT'] ?? 'Not set');
+echo "<br>Script path: " . __FILE__;
+?> 
